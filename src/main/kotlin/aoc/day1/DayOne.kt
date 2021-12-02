4a636575dc3fb3f134e317aa0b4efaf138438f28
Tim Fennis <tim.fennis@nos.nl>
2021-12-02 14:30:27 +0100

    Improved documentation

--- src/main/kotlin/aoc/day1/DayOne.kt
+++ src/main/kotlin/aoc/day1/DayOne.kt
@@ -5,6 +5,14 @@ fun main() {
     println(solveDayOnePartOne(solveDayOnePartTwo(dayOneInput)))
 }
 
-fun solveDayOnePartTwo(input: List<Int>) = input.zipWithNext().zipWithNext { a, b -> a.first + a.second + b.second }
-
+/**
+ * [1,2,3].zipWithNext() == List(Pair(1,2), Pair(2,3))
+ *
+ * After pairing all values with the next value we can count the amount of times when a < b to get the final answer
+ */
 fun solveDayOnePartOne(input: List<Int>) = input.zipWithNext().count { it.first < it.second }
+
+/**
+ * Applying zipWithNext twice and discarding the second pair first value (b.first) essentially gives us the sliding window we need.
+ */
+fun solveDayOnePartTwo(input: List<Int>) = input.zipWithNext().zipWithNext { a, b -> a.first + a.second + b.second }
