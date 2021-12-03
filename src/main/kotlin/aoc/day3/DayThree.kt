c7cb39807d30312017ebd324e8ae38a6b2e46a79
Tim Fennis <tim.fennis@nos.nl>
2021-12-03 07:57:10 +0100

    Refactored day 3, improved readability

--- src/main/kotlin/aoc/day3/DayThree.kt
+++ src/main/kotlin/aoc/day3/DayThree.kt
@@ -1,52 +1,58 @@
 package aoc.day3
 
-
-fun main () {
+fun main() {
     runOnInput(dayThreeExampleInput)
     println("\n-------------------\n")
     runOnInput(dayThreeInput)
 }
 
-fun runOnInput(input: Input)
-{
-    println("Power consumption is: " + calculateGammaAndEpsilon(input).toPowerConsumption())
-    println("Most common at 0: " + mostCommonAt(input, 0))
-    println("Oxygen rating: " + findOxygenGeneratorRating(input) + " " + findOxygenGeneratorRating(input).toLong(2))
-    println("CO2 rating: " + findCO2ScrubberRating(input) + " " + findCO2ScrubberRating(input).toLong(2))
-    println("Life support rating: " + calculateLifeSupportRating(findOxygenGeneratorRating(input),findCO2ScrubberRating(input)))
+fun runOnInput(input: Input) {
+    val gammaAndEpsilon = calculateGammaAndEpsilon(input)
+    val oxygenGeneratorRating = findOxygenGeneratorRating(input)
+    val cO2ScrubberRating = findCO2ScrubberRating(input)
+    val lifeSupportRating = calculateLifeSupportRating(oxygenGeneratorRating, cO2ScrubberRating)
+
+    println("Gamma and Epsilon: $gammaAndEpsilon")
+    println("Power consumption is: " + gammaAndEpsilon.toPowerConsumption())
+    println("Oxygen rating: $oxygenGeneratorRating [${oxygenGeneratorRating.binaryStringToLong()}]")
+    println("CO2 rating: $cO2ScrubberRating ${cO2ScrubberRating.binaryStringToLong()}")
+    println("Life support rating: $lifeSupportRating")
 }
+
+fun findOxygenGeneratorRating(input: Input)  = findRating(input, ::mostCommonBitAtOffset)
+fun findCO2ScrubberRating(input: Input) = findRating(input, ::leastCommonBitAtOffset)
+
+fun calculateGammaAndEpsilon(input: Input) = Pair(
+    input.first().mapIndexed { index, _ -> mostCommonBitAtOffset(input, index) }.join().binaryStringToLong(),
+    input.first().mapIndexed { index, _ -> leastCommonBitAtOffset(input, index) }.join().binaryStringToLong()
+)
+
+fun calculateLifeSupportRating(a: String, b: String) = a.binaryStringToLong() * b.binaryStringToLong()
+
+//
+// Support functions
+//
 
 fun bitsAt(input: Input, offset: Int): Collection<Char> = input.map { it[offset] }
 
-fun mostCommonAt(input: Input, offset: Int) =
+fun mostCommonBitAtOffset(input: Input, offset: Int) =
     if (bitsAt(input, offset).count { it == '1' } >= (input.size / 2.0)) '1' else '0'
 
-fun leastCommonAt(input: Input, offset: Int) =
+fun leastCommonBitAtOffset(input: Input, offset: Int) =
     if (bitsAt(input, offset).count { it == '0' } <= (input.size / 2.0)) '0' else '1'
 
-fun findOxygenGeneratorRating(input: Input, offset: Int = 0): String = if (input.size == 1) {
+fun findRating(input: Input, filter: (Input, Int) -> Char, offset: Int = 0): String = if (input.size == 1) {
     input.first()
 } else {
-    val filtered = filterOnPosition(input, offset, mostCommonAt(input, offset))
-    findOxygenGeneratorRating(filtered, offset + 1)
+    findRating(filterOnPosition(input, offset, filter(input, offset)), filter, offset + 1)
 }
 
-fun findCO2ScrubberRating(input: Input, offset: Int = 0): String = if (input.size == 1) {
-    input.first()
-} else {
-    findCO2ScrubberRating(filterOnPosition(input, offset, leastCommonAt(input, offset)), offset + 1)
-}
+fun filterOnPosition(input: Input, offset: Int, filter: Char) = input.filter { it[offset] == filter }
 
-fun filterOnPosition(input: Input, offset: Int, filter: Char) = input.filter { it[offset] == filter  }
+//
+// Helper extension functions
+//
 
-
-fun calculateGammaAndEpsilon(input: Input) = Pair(
-    input.first().mapIndexed { index, _ -> mostCommonAt(input, index) }.joinToString("").toLong(2),
-    input.first().mapIndexed { index, _ -> leastCommonAt(input, index) }.joinToString("").toLong(2)
-)
-
-fun calculateLifeSupportRating(a: String, b: String) = a.toLong(2) * b.toLong(2)
-
+fun String.binaryStringToLong() = this.toLong(2)
 fun Pair<Long, Long>.toPowerConsumption() = this.first * this.second
-
 fun List<Char>.join() = this.joinToString("")
