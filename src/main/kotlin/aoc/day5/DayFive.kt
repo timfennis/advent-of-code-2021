941f7c67261d7e608ebd27e7c0fbcf59794d78f4
Tim Fennis <tim.fennis@nos.nl>
2021-12-05 11:50:07 +0100

    more comments

--- src/main/kotlin/aoc/day5/DayFive.kt
+++ src/main/kotlin/aoc/day5/DayFive.kt
@@ -5,11 +5,13 @@ import kotlin.math.max
 
 fun main ()
 {
+    // In the initial version I tried to resize the grid every time a line was added that wasn't in bounds
+    // but this gave me a headache
     val width = dayFiveInput.maxByOrNull { max(it.x1, it.x2) }?.let { max(it.x1, it.x2) + 1 } ?: 10
     val height = dayFiveInput.maxByOrNull { max(it.y1, it.y2) }?.let { max(it.y1, it.y2) + 1 } ?: 10
 
     // In the lines below MutableGrid can be substituted for Grid which is slower but purely functional.
-    // Make sure to also check DayFiveOptimized.kt because it has a much simpler implementation (pure and mutable) without grid
+    // Make sure to also check DayFiveOptimized.kt because it has a much simpler implementations of the same problem
 
     // Part one
     val grid1 = dayFiveInput
