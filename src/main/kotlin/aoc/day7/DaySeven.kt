a589f30986449091522721b1f264230e162e843e
Tim Fennis <tim.fennis@nos.nl>
2021-12-07 08:26:59 +0100

    Day 7: EZPZ

---
+++ src/main/kotlin/aoc/day7/DaySeven.kt
@@ -0,0 +1,34 @@
+package aoc.day7
+
+import arrow.core.memoize
+import kotlin.math.max
+import kotlin.math.min
+
+
+fun main () {
+    println(solve(examplePositions, ::calcPartOneCost))
+    println(solve(daySevenPositions, ::calcPartOneCost))
+
+    println(solve(examplePositions, ::calcPartTwoCost))
+    println(solve(daySevenPositions, ::calcPartTwoCost))
+}
+
+fun solve(input: List<Int>, calculator: (List<Int>, Int) -> Int) =
+    (input.minByOrNull { it }!! .. input.maxByOrNull { it }!!)
+        .map { it to calculator(input, it) }
+        .minByOrNull { (_, cost) -> cost }
+
+fun calcPartOneCost(input: List<Int>, position: Int) =
+    input.sumOf { max(it, position) - min(it, position) }
+
+fun calcPartTwoCost(input: List<Int>, position: Int) =
+    input.sumOf { triangleNumber(max(it, position) - min(it, position)) }
+
+fun triangleNumber(n: Int) = ((n * n) + n) / 2
+
+/**
+ * Originally I had created this tiny beast (that was really slow without memoization)
+ * but after googling for "Factorial with addition" I found the triangleNumber formula
+ */
+val factorialButWithAddition = ::_factorialButWithAddition.memoize()
+fun _factorialButWithAddition(n: Int):  Int = if (n == 0) 0 else n + _factorialButWithAddition(n - 1)
