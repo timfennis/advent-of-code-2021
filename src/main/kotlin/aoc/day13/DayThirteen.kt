d8289f3e639195c8236cd51e44595144bd8a1ff2
Tim Fennis <tim.fennis@nos.nl>
2021-12-13 07:47:53 +0100

    Some improvements to code quality.

--- src/main/kotlin/aoc/day13/DayThirteen.kt
+++ src/main/kotlin/aoc/day13/DayThirteen.kt
@@ -3,8 +3,6 @@ package aoc.day13
 import aoc.Day
 import aoc.splitLines
 
-typealias Point = Pair<Int, Int>
-
 class DayThirteen : Day(13) {
     override val exampleSolution = listOf(17, -1L)
 
@@ -13,53 +11,59 @@ class DayThirteen : Day(13) {
         .let {
             it.folds
                 .take(1)
-                .fold(it.points) { points, fold -> points.applyFold(fold) }
-        }.toSet().size.toLong()
+                .fold(it.points) { points, fold -> applyFold(points, fold) }
+        }.size.toLong()
 
     override fun solvePartTwo(input: String) = input.split("\n\n")
         .let { InputData(it.first().toPoints(), it.last().toFolds()) }
         .let {
-            it.folds.fold(it.points) { points, fold -> points.applyFold(fold) }
+            it.folds.fold(it.points) { points, fold -> applyFold(points, fold) }
 
         }
-        .also { println(it) }
-        .also { (0..5).forEach { y -> (0..40).forEach { x -> print(if (it.contains(x to y)) 'X' else '.') }; println() } }
+        .also {
+            println((0..5).joinToString("\n") { y ->
+                (0..40).joinToString("") { x ->
+                    if (it.contains(Point(x, y))) "█" else " "
+                }
+            })
+        }
         .let { -1L }
-        .toLong()
 }
 
 private fun String.toPoints() =
-    this.splitLines().map { it.split(',').first().toInt() to it.split(',').last().toInt() }
+    this.splitLines().map { Point(it.split(',').first().toInt(), it.split(',').last().toInt()) }.toSet()
 
 private fun String.toFolds() =
     this.splitLines().map { Fold(it.split('=').first().last(), it.split('=').last().toInt()) }
 
-private fun List<Point>.applyFold(fold: Fold) =
-    this.fold(emptyList<Point>()) { points, point -> points + point.foldOver(fold) }
+private fun applyFold(set: Set<Point>, fold: Fold) =
+    set.fold(emptySet<Point>()) { points, point -> points + point.foldOver(fold) }
 
-data class InputData(val points: List<Point>, val folds: List<Fold>)
+data class InputData(val points: Set<Point>, val folds: List<Fold>)
 data class Fold(val direction: Char, val coordinate: Int)
+data class Point(val x: Int, val y: Int) {
 
-fun Point.foldOver(fold: Fold) = if (fold.direction == 'x') {
-    if (fold.coordinate == this.first) {
-        // This case never actually happens on the input data
-        emptyList()
-    } else {
-        if (this.first > fold.coordinate) {
-            listOf(Pair(fold.coordinate - (this.first - fold.coordinate), this.second))
+    fun foldOver(fold: Fold): Set<Point> = if (fold.direction == 'x') {
+        if (fold.coordinate == this.x) {
+            // This case never actually happens on the input data
+            emptySet()
         } else {
-            listOf(this)
+            if (this.x > fold.coordinate) {
+                setOf(Point(fold.coordinate - (this.x - fold.coordinate), this.y))
+            } else {
+                setOf(this)
+            }
         }
-    }
-} else {
-    if (fold.coordinate == this.second) {
-        // This case never actually happens on the input data
-        emptyList()
     } else {
-        if (this.second > fold.coordinate) {
-            listOf(Pair(this.first, fold.coordinate - (this.second - fold.coordinate)))
+        if (fold.coordinate == this.y) {
+            // This case never actually happens on the input data
+            emptySet()
         } else {
-            listOf(this)
+            if (this.y > fold.coordinate) {
+                setOf(Point(this.x, fold.coordinate - (this.y - fold.coordinate)))
+            } else {
+                setOf(this)
+            }
         }
     }
 }
