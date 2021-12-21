a06ca186835566e602213f89c42d93f9bfc62250
Tim Fennis <tim.fennis@nos.nl>
2021-12-21 18:19:08 +0100

    Optimizations zo amazing I barely understand them

--- src/main/kotlin/aoc/day21/SimpleApproach.kt
+++ src/main/kotlin/aoc/day21/SimpleApproach.kt
@@ -1,30 +1,29 @@
 package aoc.day21
 
+import arrow.core.foldLeft
 import arrow.core.memoize
 import kotlin.math.max
 
 fun main (){
-    val (a, b) = sim(5,10, 0, 0, true)
+    val (a, b) = sim(5-1,10-1, 0, 0)
     println("p1 $a p2 $b best ${max(a, b)}")
 }
 
-val sim = ::simulate.memoize()
+// val sim = ::simulate.memoize()
 
-fun simulate(currentPlayer: Int, otherPlayer: Int, currentScore: Int, otherScore: Int, p1t: Boolean): Pair<Long, Long> {
+fun sim(currentPos: Int, otherPos: Int, currentScore: Int, otherScore: Int): Pair<Long, Long> {
 
-    if (otherScore >= 21) {
-        return if (p1t) {
-            0L to 1L
-        } else {
-            1L to 0L
-        }
+    if (currentScore >= 21) {
+        return 1L to 0L
+    } else if (otherScore >= 21) {
+        return 0L to 1L
     }
 
-    return outcomes.map { outcome ->
-        val v1 = (currentPlayer + outcome) % 10
-        val v2 = if (v1 == 0) 10 else v1
-        sim(otherPlayer, v2, otherScore, currentScore + v2, !p1t)
-    }.fold(0L to 0L) { (x, y), (xx, yy) -> x + xx to y + yy }
+    return outcomes.foldLeft(0L to 0L) { (x, y), (outcome, times) ->
+        val newPos = (currentPos + outcome) % 10
+        val (xx, yy) = sim(otherPos, newPos, otherScore, currentScore + newPos + 1)
+        x + (yy * times) to y + (xx * times)
+    }
 }
 
 
@@ -34,4 +33,4 @@ private val outcomes = listOf(1,2,3).flatMap { x ->
             listOf(x,y,z).sum()
         }
     }
-}
+}.groupBy { it }.mapValues { (_, v) -> v.size }
